616f02ffac87d6d6bb8bad5b971a0639dc30b455
waaaaall <nm7wall@gmail.com>
2020-01-23 18:21:17 +0900

    fix

--- js/enviroment.js
+++ js/enviroment.js
@@ -12,7 +12,7 @@ if (env == "develop") {
 function envMeta() {
   document.open();
   document.write(
-    '<title>DEVELOP</title><meta property="og:type" content="website" /><meta property="og:site_name" content="develop" /><meta property="og:title" content="develop" /><meta property="og:description" content="JavaScriptでのOGPの出し分け確認環境" /><meta property="og:url" content="https://develop.cpdev.jp/" /><meta property="og:image" content="https://develop.cpdev.jp/img/ogp.png" /><meta property="og:locale" content="ja_JP" /><meta name="twitter:card" content="summary_large_image" /><meta name="twitter:title" content="develop" /><metaname="twitter:description"content="JavaScriptでのOGPの出し分け確認環境" /><meta name="twitter:url" content="https://develop.cpdev.jp/" /><meta name="twitter:image" content="https://develop.cpdev.jp/img/develop.png" /><meta name="description" content="JavaScriptでのOGPの出し分け確認環境" />'
+    '<title>DEVELOP</title><meta property="og:type" content="website" /><meta property="og:site_name" content="develop" /><meta property="og:title" content="develop" /><meta property="og:description" content="JavaScriptでのOGPの出し分け確認環境" /><meta property="og:url" content="https://develop.cpdev.jp/" /><meta property="og:image" content="https://develop.cpdev.jp/img/ogp.png" /><meta property="og:locale" content="ja_JP" /><meta name="twitter:card" content="summary_large_image" /><meta name="twitter:title" content="develop" /><meta name="twitter:description" content="JavaScriptでのOGPの出し分け確認環境"/><meta name="twitter:url" content="https://develop.cpdev.jp/" /><meta name="twitter:image" content="https://develop.cpdev.jp/img/develop.png" /><meta name="description" content="JavaScriptでのOGPの出し分け確認環境" />'
   );
   document.close();
 }
