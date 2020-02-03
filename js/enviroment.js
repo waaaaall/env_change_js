9c4bfdb542ba47324d2c5700b20ed2bca5d47fc0
waaaaall <nm7wall@gmail.com>
2020-02-03 16:40:24 +0900

    functionに処理を書き出し

--- js/enviroment.js
+++ js/enviroment.js
@@ -1,30 +1,14 @@
 if (env == "production") {
-  //   console.log("本番");
   var rootPath = "https://production.cpdev.jp";
 }
 if (env == "staging") {
-  //   console.log("検証");
+  console.log("検証");
   var rootPath = "https://staging.cpdev.jp";
 }
 if (env == "develop") {
-  //   console.log("開発");
+  console.log("開発");
   var rootPath = "https://develop.cpdev.jp";
 }
 
-const metaDiscre = document.head.children;
-const metaLength = metaDiscre.length;
-for (var i = 0; i < metaLength; i++) {
-  var proper = metaDiscre[i].getAttribute("name");
-  if (proper === "description") {
-    const dis = metaDiscre[i];
-    dis.setAttribute("content", rootPath);
-    console.log(dis);
-  }
-  var proper = metaDiscre[i].getAttribute("property");
-  if (proper === "og:image") {
-    var dir = metaDiscre[i].getAttribute("content");
-    const dis = metaDiscre[i];
-    dis.setAttribute("content", rootPath + dir);
-    console.log(dis);
-  }
-}
+let proper = ["og:url", "og:image"];
+envChange(rootPath, proper);
